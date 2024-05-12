5bbf8d0d2425f1ad2241c3067518bb5f551dfbb1
JOYRATAR <lekju0505@gmail.com>
2024-05-12 23:32:24 +0700

    semangattt tugas tiga

--- int_trap.m
+++ int_trap.m
@@ -1,8 +1,8 @@
-function int = int_trap(func, a, b, n)
+function int = int_trap(f, a, b, n)
 
     h = (b - a) / n;
     x = a + (0:n) * h;
-    y = func(x);
+    y = f(x);
 
     int = (h / 2) * (y(1) + 2 * sum(y(2:end-1)) + y(end));
 end
